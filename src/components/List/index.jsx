f6fda65116a87036c3c1b5f09d703ea9f3bff19a
Luiza Gadelha <89354119+LuizaGA@users.noreply.github.com>
2022-05-30 17:15:00 -0300

    modal pronto

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Table } from 'react-bootstrap';
 import api from '../../services/api'
 import moment from 'moment';
-import { ListContainer, ViewButton, BotaoCarregarDados, TextoBotaoDados, FundoModal, ContainerModal} from "./style";
+import { ListContainer, ViewButton, BotaoCarregarDados, TextoBotaoDados, FundoModal, ContainerModal, Imagem} from "./style";
 import Loading from '../Loading'
 
 export default function List() {
@@ -94,16 +94,16 @@ export default function List() {
     {modal ? (
       <FundoModal onClick={() => showModal()}>
         <ContainerModal>
-          <p>Imagem: {}</p>
+          <img src={oneData.picture.medium} />
           <p>Nome completo: {oneData.name.first} {oneData.name.last}</p>
           <p>Email: {oneData.email}</p>
           <p>Gênero: {oneData.gender}</p>
           <p>Data de naascimento: {moment(oneData.dob.date).format('DD/MM/YYYY')}</p>
           <p>Telefone: {oneData.phone}</p>
-          <p>Nacionalidade: {}</p>
-          <p>Endereço: {}</p>
-          <p>ID (número de identificação): {}</p>
-          <p>URL para compartilhamento: {}</p>
+          <p>Nacionalidade: {oneData.nat}</p>
+          <p>Endereço: {oneData.location.street.name}, {oneData.location.street.number}</p>
+          <p>ID (número de identificação): {oneData.id.value}</p>
+          <p>URL para compartilhamento: {oneData.picture.large}</p>
         </ContainerModal>
       </FundoModal>)
     : null}
